2ad4023be91458bb8959362d2d500cfc28717ce8
jlhuns <joshua.l.hunsaker@gmail.com>
2024-09-07 11:26:32 -0600

    Added functionality to class Chess board using a HashMap<ChessPosition, ChessPiece>(); to keep track of the board. Added functions: addPiece, getPiece to pass test: getAddPiece()

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,4 +1,7 @@
 package chess;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -7,9 +10,10 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private final Map<ChessPosition, ChessPiece> board;
 
     public ChessBoard() {
-        
+        this.board = new HashMap<ChessPosition, ChessPiece>();
     }
 
     /**
@@ -19,7 +23,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        this.board.put(position, piece);
     }
 
     /**
@@ -30,7 +34,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return this.board.get(position);
     }
 
     /**
