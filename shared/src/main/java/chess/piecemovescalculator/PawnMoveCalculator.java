5b57760bbe5af467440b8d5d8952f3f0c197882f
jlhuns <joshua.l.hunsaker@gmail.com>
2024-09-09 13:33:31 -0600

    refactored pawnMoveCalculator

--- shared/src/main/java/chess/piecemovescalculator/PawnMoveCalculator.java
+++ shared/src/main/java/chess/piecemovescalculator/PawnMoveCalculator.java
@@ -7,19 +7,43 @@ import java.util.Collection;
 import java.util.List;
 
 public class PawnMoveCalculator implements PieceMovesCalculator {
-    private final ChessPiece piece;
+    private final ChessGame.TeamColor teamColor;
+    private final int startRow;
+    private final int moveDirection;
+    private final int promotionRow;
+    private final int[][] diagonalMoveDirection;
+
+    public PawnMoveCalculator(ChessPiece piece) {
+        this.teamColor = piece.getTeamColor();
+
+        if (teamColor == ChessGame.TeamColor.WHITE){
+            this.startRow = 2;
+            this.promotionRow = 8;
+            this.moveDirection = 1;
+            this.diagonalMoveDirection = new int[][]{
+                    {1,-1},
+                    {1, 1}
+            };
+
+        }else{
+            this.startRow = 7;
+            this.promotionRow = 1;
+            this.moveDirection = -1;
+            this.diagonalMoveDirection = new int[][]{
+                    {-1,-1},
+                    {-1,1}
+            };
+
+        }
+    }
+
+    private boolean canPromote(ChessPosition newPosition){
+        return newPosition.getRow() == promotionRow;
+    }
+
     @Override
     public int[][] getMoveDirection(){
         return new int[][]{};
-    }
-
-    public PawnMoveCalculator(ChessPiece piece) {
-        this.piece = piece;
-    }
-
-    private boolean isPromotionRow(int row) {
-        return (piece.getTeamColor() == ChessGame.TeamColor.WHITE && row == 8) ||
-                (piece.getTeamColor() == ChessGame.TeamColor.BLACK && row == 1);
     }
 
     @Override
@@ -27,61 +51,47 @@ public class PawnMoveCalculator implements PieceMovesCalculator {
         List<ChessMove> moves = new ArrayList<>();
         int row = position.getRow();
         int column = position.getColumn();
-        ChessGame.TeamColor color = piece.getTeamColor();
 
-        // White moves up, black moves down
-        int moveDirection = color == ChessGame.TeamColor.WHITE ? 1 : -1;
-
-        // Single square move
-        ChessPosition oneStepForward = new ChessPosition(row + moveDirection, column);
-        if (isValidPosition(oneStepForward.getRow(), oneStepForward.getColumn()) && board.getPiece(oneStepForward) == null && !isPromotionRow(row + moveDirection)) {
-            moves.add(new ChessMove(position, oneStepForward, null));
-        }
-
-        // Double square move
-        int startRow = color == ChessGame.TeamColor.WHITE ? 2 : 7;
-        if (row == startRow && board.getPiece(oneStepForward) == null) {
-            ChessPosition twoStepForward = new ChessPosition(row + (2 * moveDirection), column);
-            if (isValidPosition(twoStepForward.getRow(), twoStepForward.getColumn()) && board.getPiece(twoStepForward) == null && !isPromotionRow(row + moveDirection)) {
-                moves.add(new ChessMove(position, twoStepForward, null));
+        //one square move
+        ChessPosition oneSquareMove = new ChessPosition(row + moveDirection, column);
+        ChessPiece pieceAtNewPosition = board.getPiece(oneSquareMove);
+        if (pieceAtNewPosition == null) {
+            if (canPromote(oneSquareMove)) {
+                // Promotion to Queen, Rook, Bishop, Knight
+                moves.add(new ChessMove(position, oneSquareMove, ChessPiece.PieceType.QUEEN));
+                moves.add(new ChessMove(position, oneSquareMove, ChessPiece.PieceType.ROOK));
+                moves.add(new ChessMove(position, oneSquareMove, ChessPiece.PieceType.BISHOP));
+                moves.add(new ChessMove(position, oneSquareMove, ChessPiece.PieceType.KNIGHT));
+            } else {
+                // Regular move
+                moves.add(new ChessMove(position, oneSquareMove, null));
             }
         }
-
-        // Diagonal capture
-        int[][] diagonalCaptureDirection = {
-                {moveDirection, -1}, // capture left
-                {moveDirection, 1}   // capture right
-        };
-        for (int[] diagonal : diagonalCaptureDirection) {
-            int newRow = row + diagonal[0];
-            int newColumn = column + diagonal[1];
-            if (isValidPosition(newRow, newColumn)) {
+        //two square move
+        if(row == startRow) {
+            ChessPosition twoSquareMove = new ChessPosition(row + (2 * moveDirection), column);
+            ChessPiece pieceAtNewPosition2 = board.getPiece(twoSquareMove);
+            if (pieceAtNewPosition2 == null && pieceAtNewPosition == null) {
+                moves.add(new ChessMove(position, twoSquareMove, null));
+            }
+        }
+        //diagonal capture
+        for (int[] diagonalDirection : diagonalMoveDirection){
+            int newRow = row + diagonalDirection[0];
+            int newColumn = column + diagonalDirection[1];
+            if(isValidPosition(newRow, newColumn)){
                 ChessPosition newPosition = new ChessPosition(newRow, newColumn);
-                ChessPiece capturePiece = board.getPiece(newPosition);
-                if (capturePiece != null && capturePiece.getTeamColor() != color) {
-                    if(row == (color == ChessGame.TeamColor.WHITE ? 7 : 2)){
+                ChessPiece newPieceAtNewPosition = board.getPiece(newPosition);
+                if(newPieceAtNewPosition != null && newPieceAtNewPosition.getTeamColor() != teamColor){
+                    if (canPromote(newPosition)) {
                         moves.add(new ChessMove(position, newPosition, ChessPiece.PieceType.QUEEN));
                         moves.add(new ChessMove(position, newPosition, ChessPiece.PieceType.ROOK));
-                        moves.add(new ChessMove(position, newPosition, ChessPiece.PieceType.BISHOP));
                         moves.add(new ChessMove(position, newPosition, ChessPiece.PieceType.KNIGHT));
+                        moves.add(new ChessMove(position, newPosition, ChessPiece.PieceType.BISHOP));
                     }else{
                         moves.add(new ChessMove(position, newPosition, null));
                     }
-
                 }
-            }
-        }
-
-        // Promotion
-        if (row == (color == ChessGame.TeamColor.WHITE ? 7 : 2)) {
-            // Promotion to Queen, Rook, Bishop, Knight
-            ChessPosition promotionPosition = new ChessPosition(row + moveDirection, column);
-            if (isValidPosition(promotionPosition.getRow(), promotionPosition.getColumn())) {
-                // Add promotion moves to all four types
-                moves.add(new ChessMove(position, promotionPosition, ChessPiece.PieceType.QUEEN));
-                moves.add(new ChessMove(position, promotionPosition, ChessPiece.PieceType.ROOK));
-                moves.add(new ChessMove(position, promotionPosition, ChessPiece.PieceType.BISHOP));
-                moves.add(new ChessMove(position, promotionPosition, ChessPiece.PieceType.KNIGHT));
             }
         }
 
