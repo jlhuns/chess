aac1118351e258860fbf137f366b7330bfb6b444
jlhuns <joshua.l.hunsaker@gmail.com>
2024-10-20 21:09:43 -0600

    added data models

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,20 @@
+package model;
+
+import chess.ChessGame;
+
+import java.util.Objects;
+
+public record GameData(int gameID, String whiteUsername, String blackUsername, ChessGame game) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameData gameData = (GameData) o;
+        return gameID == gameData.gameID && Objects.equals(game, gameData.game) && Objects.equals(whiteUsername, gameData.whiteUsername) && Objects.equals(blackUsername, gameData.blackUsername);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameID, whiteUsername, blackUsername, game);
+    }
+}
