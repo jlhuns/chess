7ee7566282250baffd2056c6d018ae5c0f1415fa
jlhuns <joshua.l.hunsaker@gmail.com>
2024-10-20 20:51:03 -0600

    Reapply "added files"
    This reverts commit 5e4d954e935daa562bbac29b8f11a742df84de0a.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,5 +1,30 @@
 import chess.*;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
+public class Server {
+    private HttpServer server;
+
+    public void run(int port) {
+        try {
+            server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/", httpExchange -> {
+                String response = "♕ Welcome to 240 Chess Server!";
+                httpExchange.sendResponseHeaders(200, response.getBytes().length);
+                try (var os = httpExchange.getResponseBody()) {
+                    os.write(response.getBytes());
+                }
+            });
+            server.start();
+            System.out.println("Server started on port " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
