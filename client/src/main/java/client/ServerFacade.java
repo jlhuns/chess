330793a53354c5ee2a8fc8fb14e8948c59cc72f2
jlhuns <joshua.l.hunsaker@gmail.com>
2024-12-03 13:02:51 -0700

    messages classes and empty command classes

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,6 +1,8 @@
 package client;
 
+import com.google.gson.Gson;
 import model.GameData;
+import websocket.commands.UserGameCommand;
 
 import java.util.List;
 
@@ -56,6 +58,30 @@ public class ServerFacade {
         catch (Exception e) {
             System.out.println("Failed to make connection with server");
         }
+    }
+    public void sendCommand(UserGameCommand command) {
+        String message = new Gson().toJson(command);
+        ws.sendMessage(message);
+    }
+
+    public void joinPlayer(int gameID, ChessGame.TeamColor color) {
+        sendCommand(new JoinPlayer(authToken, gameID, color));
+    }
+
+    public void joinObserver(int gameID) {
+        sendCommand(new JoinObserver(authToken, gameID));
+    }
+
+    public void makeMove(int gameID, ChessMove move) {
+        sendCommand(new MakeMove(authToken, gameID, move));
+    }
+
+    public void leave(int gameID) {
+        sendCommand(new Leave(authToken, gameID));
+    }
+
+    public void resign(int gameID) {
+        sendCommand(new Resign(authToken, gameID));
     }
 }
 
