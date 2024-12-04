b86cac199df2427f21cfbf6a457b059f95e722f1
jlhuns <joshua.l.hunsaker@gmail.com>
2024-12-04 10:32:05 -0700

    messages received from server

--- client/src/main/java/ui/GamePlayREPL.java
+++ client/src/main/java/ui/GamePlayREPL.java
@@ -43,16 +43,31 @@ public class GamePlayREPL {
         while(isInGame){
             String[] input = getUserInput();
             switch(input[0]){
+                case "help":
+                    printHelpMenu();
+                    break;
                 case "quit":
                     return;
                 case "leave":
                     isInGame = false;
+                    server.leave(gameID);
+                    break;
+                case "Redraw":
                     break;
                 case "Make Move":
-
+                    break;
                 case "Resign":
-
-
+                    out.println("Are you sure you want to forfeit? (yes/no)");
+                    String[] confirmation = getUserInput();
+                    if (confirmation.length == 1 && confirmation[0].equalsIgnoreCase("yes")) {
+                        server.resign(gameID);
+                    }
+                    else {
+                        out.println("Resignation cancelled");
+                    }
+                    break;
+                case "Highlight":
+                    break;
                 default:
                     out.println("Will implement more commands in phase 6 - for now just quit or leave");
                     break;
@@ -61,6 +76,23 @@ public class GamePlayREPL {
         if(!isInGame){
             postloginREPL.run();
         }
+    }
+
+    private void printHelpMenu(){
+        out.println("redraw - redraw the game board");
+        out.println("leave - leave the current game");
+        printMakeMove();
+        out.println("resign - forfeit this game");
+        printHighlight();
+        out.println("help - show this menu");
+    }
+
+    private void printMakeMove() {
+        out.println("move <from> <to> <promotion_piece> - make a move (Promotion piece should only be used when a move will promote a pawn)");
+    }
+
+    private void printHighlight() {
+        out.println("highlight <coordinate> - highlight all legal moves for the given piece");
     }
 
     private String[] getUserInput() {
