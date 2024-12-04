e5f9be40182105425e637d9142b1588eca890f68
jlhuns <joshua.l.hunsaker@gmail.com>
2024-12-04 15:40:29 -0700

    can move but it moves wrong piceses

--- client/src/main/java/ui/GamePlayREPL.java
+++ client/src/main/java/ui/GamePlayREPL.java
@@ -85,8 +85,9 @@ public class GamePlayREPL {
     }
     private void handleMakeMove(String[] input) {
         if (input.length >= 3 && input[1].matches("[a-h][1-8]") && input[2].matches("[a-h][1-8]")) {
-            ChessPosition from = new ChessPosition(input[1].charAt(1) - '0', input[1].charAt(0) - ('a'-1));
-            ChessPosition to = new ChessPosition(input[2].charAt(1) - '0',input[2].charAt(0) - ('a'-1));
+            ChessPosition from = new ChessPosition(9 - (input[1].charAt(1) - '0'), input[1].charAt(0) - ('a' - 1));
+            ChessPosition to = new ChessPosition(9 - (input[2].charAt(1) - '0'), input[2].charAt(0) - ('a' - 1));
+
 
             ChessPiece.PieceType promotion = null;
             if (input.length == 4) {
@@ -136,11 +137,5 @@ public class GamePlayREPL {
         out.print("\n[In-Game] >>> ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine().split(" ");
-    }
-    public BoardPrint getBoardPrint() {
-        return boardPrint;
-    }
-    public ChessGame.TeamColor getColor() {
-        return color;
     }
 }
